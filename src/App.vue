4d35e66d934bbd6c74cbc2765487e36c3a0a1e06
VictoriyaK19 <kalinovavictoriya@gmail.com>
2023-10-26 16:45:32 +0300

    Adding More State

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,43 @@
 <template>
-  <base-container title="Vuex">
-  <the-counter></the-counter>
-  <favourite-value></favourite-value>
-  <button @click="addOne">Add 10</button>
-  <change-counter></change-counter>
+  <base-container title="Vuex" v-if="isAuth">
+    <the-counter></the-counter>
+    <favourite-value></favourite-value>
+    <button @click="addOne">Add 10</button>
+    <change-counter></change-counter>
+  </base-container>
+  <base-container title="Auth">
+    <user-auth></user-auth>
   </base-container>
 </template>
 
 <script>
-import BaseContainer from './components/BaseContainer.vue';
-import TheCounter from './components/TheCounter.vue';
-import ChangeCounter from './components/ChangeCounter.vue';
-import FavouriteValue from './components/FavouriteValue.vue';
+import BaseContainer from "./components/BaseContainer.vue";
+import TheCounter from "./components/TheCounter.vue";
+import ChangeCounter from "./components/ChangeCounter.vue";
+import FavouriteValue from "./components/FavouriteValue.vue";
+import UserAuth from "./components/UserAuth.vue";
 
 export default {
   components: {
     BaseContainer,
     TheCounter,
     ChangeCounter,
-    FavouriteValue
+    FavouriteValue,
+    UserAuth,
   },
-  
+  computed: {
+    isAuth() {
+      return this.$store.getters.userIsAuthenticated;
+    },
+  },
+
   methods: {
     addOne() {
       // this.$store.dispatch('increase', {value: 10});
       this.$store.dispatch({
-        type: 'increase',
-        value: 10
-      })
+        type: "increase",
+        value: 10,
+      });
     },
   },
 };
